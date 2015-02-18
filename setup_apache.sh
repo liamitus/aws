c5fb1ca2a007872a060b79b2693259acdc236300
Liam Howell <liammm@gmail.com>
2015-02-18 00:07:36 -0500

    fixed a typo

--- setup_apache.sh
+++ setup_apache.sh
@@ -2,6 +2,6 @@ sudo yum update -y
 sudo yum groupinstall -y "Web Server"
 sudo service httpd start
 sudo groupadd www
-sudo usermod -a -G www ec2-usermod
+sudo usermod -a -G www ec2-user
 echo "Please exit the terminal, reconnect, and then run the following:"
 echo "curl \"https://raw.githubusercontent.com/liamitus/aws/master/setup_apache_permissions.sh\" | sudo sh"
