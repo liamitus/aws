b26c9b2046208574682e4f5a1a471d703af174e0
Liam Howell <liammm@gmail.com>
2015-02-18 00:06:38 -0500

    updated apache setup

--- setup_apache.sh
+++ setup_apache.sh
@@ -1,4 +1,7 @@
 sudo yum update -y
 sudo yum groupinstall -y "Web Server"
 sudo service httpd start
-
+sudo groupadd www
+sudo usermod -a -G www ec2-usermod
+echo "Please exit the terminal, reconnect, and then run the following:"
+echo "curl \"https://raw.githubusercontent.com/liamitus/aws/master/setup_apache_permissions.sh\" | sudo sh"
