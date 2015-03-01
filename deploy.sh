0ade71aedb53236d13948872aaada45992c3c75b
Liam Howell <liammm@gmail.com>
2015-03-01 16:10:43 -0500

    applied DRY and some cleaner abstraction

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,12 @@
 PROPERTIES_FILE="defaults.properties"
 [[ -f $PROPERTIES_FILE ]] && source $PROPERTIES_FILE
 
-ssh -t -i $PERMISSIONS_FILE_LOCATION ec2-user@$ADDRESS "sudo yum install git -y; curl \"https://raw.githubusercontent.com/liamitus/aws/master/deploy_from_github.sh\" | sudo sh -s $GITHUB_USER $GITHUB_REPO"
+# Helper function to ssh into the server and run an arbitrary string:
+run () {
+  ssh -t -i $PERMISSIONS_FILE_LOCATION $SSH_USERNAME@$ADDRESS $1
+}
+
+run "sudo yum update -y"
+run "sudo yum install git -y; curl \"https://raw.githubusercontent.com/liamitus/aws/master/deploy_from_github.sh\" | sudo sh -s $GITHUB_USER $GITHUB_REPO"
+run "sudo cp -r $GITHUB_REPO/* $SITE_DIR/; sudo rm -rf -- $SITE_DIR"
+run "sudo service httpd restart"
--- deploy_from_github.sh
+++ deploy_from_github.sh
@@ -1,5 +1,2 @@
 sudo yum install git -y
-rm -R $2
 git clone https://github.com/$1/$2.git
-sudo cp -r $2/* /var/www/html/
-#sudo service httpd restart
